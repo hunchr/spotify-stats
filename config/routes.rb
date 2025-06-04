feae665cc806df0a4c85e02a49f836d6a6f30175
Chris <76159444+hunchr@users.noreply.github.com>
2025-06-04 20:17:06 +0000

    Add artists show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   root "home#show"
 
+  resources :artists, only: %i[show]
   resources :imports, only: %i[new create]
   resources :songs, only: %i[index show] do
     get :on_repeat, on: :collection
