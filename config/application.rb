83046b483040471e52bfad3c9ae9bc2de1b901f6
Chris <76159444+hunchr@users.noreply.github.com>
2025-10-05 09:15:18 +0000

    Fix boot

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-ENV["BUNDLE_GEMFILE"] ||= File.expand_path "../Gemfile", __dir__
-
-require "bundler/setup"
+require_relative "boot"
 require "rails"
 
 require "action_controller/railtie"
